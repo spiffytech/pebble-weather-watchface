6e82d852c8d41fd86b21c20fb9e2e6b4a21bc4bb
spiffytech <spiffytech@gmail.com>
2016-03-21 11:25:35 -0400

    Cache forecast values

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ var _ = require("lodash");
 var BACKGROUND_COLOR = "white";
 var TEXT_COLOR = "black";
 
+var FORECAST_API_KEY = "";
+var LOCATION = "35.9333,-79.0333";
+
+function get_and_cache(key, duration, fn, cb) {
+  function update_value() {
+    console.log("Refreshing " + key);
+    fn(function(value) {
+      var to_store = {
+        timestamp: Date.now(),
+        value: value
+      };
+      localStorage.setItem(key, JSON.stringify(to_store));
+      
+      cb(value);
+    });
+  }
+  
+  var storedJSON = localStorage.getItem(key);
+  if(storedJSON === null) {
+    console.log("Value not cached");
+    update_value();
+  } else {
+    var stored = JSON.parse(storedJSON);
+  
+    // Set if unset / expired
+    if(
+      !stored.hasOwnProperty("timestamp") ||
+      stored.timestamp + duration <= Date.now()
+    ) {
+      console.log("Invalid/expired cached value");
+      update_value();
+    } else {
+      console.log("Reading cached value for " + key);
+      cb(stored.value);
+    }
+  }
+}
+
 // Clock
 var wind = new UI.Window({fullscreen: true});
 //var wind = new UI.Window({backgroundColor: BACKGROUND_COLOR});
@@ -59,7 +97,7 @@ function find_closest_forecast(curr_time, forecasts) {
 function retrieve_temperature(cb) {
   ajax(
     {
-      url: "https://api.forecast.io/forecast//35.9333,-79.0333",
+      url: "https://api.forecast.io/forecast/" + FORECAST_API_KEY + "/" + LOCATION,
       type: "json"
     },
     function(data) {
@@ -69,17 +107,23 @@ function retrieve_temperature(cb) {
   );
 }
 
+function cached_retrieve_temperature(cb) {
+  return get_and_cache("temperature", 1000 * 60 * 120, retrieve_temperature, cb);
+}
+
 // Display the updated temperature
 function update_temperature() {
-  retrieve_temperature(function(data) {
+  cached_retrieve_temperature(function(data) {
     var closest = find_closest_forecast(Math.floor(Date.now() / 1000), data.hourly.data);
     temperature_element.text(Math.round(closest.temperature) + " F");
     
     var d = new Date();
-    last_update_element.text(d.getHours() + ":" + d.getMinutes());
+    // The slicing is to enable zero-padding the numbers to 2 digits
+    // TODO: This doesn't get set correctly when reading a cached forecast
+    // last_update_element.text(("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2));
   });
 }
 
 // Get temperature, and schedule future updates
 update_temperature();
-setInterval(update_temperature, 1000 * 60 * 120);
+setInterval(update_temperature, 1000 * 60 * 30);
